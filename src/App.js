287e7e7b6605f19263e58364adcee07ab5d1ea8d
Anton <antonreva846@gmail.com>
2022-09-30 19:59:22 +0300

    section4 - done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./components/Main";
 import Section2 from "./components/Section2";
 import Section1 from "./components/Section1";
 import Section3 from "./components/Section3";
+import Section4 from "./components/Section4";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                 <Section1 />
                 <Section2 />
                 <Section3 />
+                <Section4 />
             </Main>
         </>
     )
---
+++ src/components/Section4.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {services} from "../data/servicesData";
+
+const Section4 = () => {
+    return (
+        <section className='services-section'>
+            <div className="container">
+                <div className="services-body">
+                    <div className='services-block'>
+                        <h1 className='services-title title'>Что мы предлогаем?</h1>
+                        <div className='services-list'>
+                            {
+                                services.map((item, ind) => (
+                                    <div key={ind} className='services-list__item'>
+                                        <div className='mobile-gutter'>
+                                            <div className='services-list__item-text__group'>
+                                                <h3 className='services-list__item-title'>{item.title}</h3>
+                                                <p className='services-list__item-text'>{item.text}</p>
+                                            </div>
+                                            <button className='services-list__item-btn btn-whtB'>Подробнее</button>
+                                        </div>
+                                        <div className='services-list__item-mobileImg-block'>
+                                            <img src={item.img} className='services-list__item-mobileImg'/>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div className='services-bg'>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Section4
