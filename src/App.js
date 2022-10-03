93c48c48b1ea40af12a4bbed086c585c590c9de5
Anton <antonreva846@gmail.com>
2022-10-03 20:15:41 +0300

    main-form-section - done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Section5 from "./components/Section5";
 import Section6 from "./components/Section6";
 import Section7 from "./components/Section7";
 import Section8 from "./components/Section8";
+import Section9 from "./components/Section9";
 
 const App = () => {
     return (
@@ -24,6 +25,7 @@ const App = () => {
                 <Section6 />
                 <Section7 />
                 <Section8 />
+                <Section9 />
             </Main>
         </>
     )
---
+++ src/components/Section9.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import CustomInput from "./CustomInput";
+
+const Section9 = () => {
+    return (
+        <section className='form-section'>
+            <div className="container">
+                <div className='form-section__body'>
+                    <div className="form-block">
+                        <div className='form-wrap'>
+                            <h1 className='main-form__title title-black'>Нужна консультация?</h1>
+                            <form className="main-form">
+                                <CustomInput inputId='main-form__input-name' placeholder='Как к вам обращаться'/>
+                                <CustomInput inputId='main-form__input-phone' placeholder='Номер телефона'/>
+                                <button className='main-form__submit-btn btn-blk'>Заказать звонок</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Section9
