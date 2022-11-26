4d7d32713e6085c3574d65f8097b5dd06b633cb8
Anton <antonreva846@gmail.com>
2022-11-26 22:24:24 +0200

    works portfolio page - done

--- src/components/FilteredPortfolioWorks.js
+++ src/components/FilteredPortfolioWorks.js
@@ -1,15 +1,52 @@
 import React from 'react'
+import {AnimatePresence, motion} from "framer-motion";
 
 export const FilteredPortfolioWorks = ({works}) => {
+
+    const cardAnim = {
+        hidden: {
+            opacity: 0
+        },
+        animate: {
+            backgroundColor: 'rgba(0, 0, 0, .5)',
+            opacity: 1,
+            transition: {
+                duration: .5
+            }
+        }
+    }
+
     return (
         <div className='worksPrtf-works__list'>
-            {
-                works.map((work, ind) => (
-                    <div className='worksPrtf-works__list-item' key={ind}>
-                        <img src={work.img} alt="" className='worksPrtf-works__list-item__img'/>
-                    </div>
-                ))
-            }
+            <AnimatePresence
+
+            >
+                {
+                    works.map((work, ind) => (
+                        <motion.div
+                            className='worksPrtf-works__list-item'
+                            key={ind}
+                            layout
+                            animate={{ opacity: 1 }}
+                            initial={{ opacity: 0 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5 }}
+
+                        >
+                            <motion.div
+                                className='worksPrtf-works__list-item__body'
+                                initial={'hidden'}
+                                whileHover={'animate'}
+                            >
+                                <img src={work.img} alt="" className='worksPrtf-works__list-item__img'/>
+                                <motion.div variants={cardAnim} className='worksPrtf-works__list__hover-block'>
+                                    {work.title}
+                                </motion.div>
+                            </motion.div>
+                        </motion.div>
+                    ))
+                }
+            </AnimatePresence>
         </div>
     )
 }
